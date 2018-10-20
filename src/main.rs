330d50e518947ea1a0f9c2e15e203de412ada676
Alex Yang <aleozlx@gmail.com>
2018-10-19 23:30:33 -0500

    + count_possibilities()

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,37 @@
 
-fn eliminate_h(dom_view: &mut [u8], idx: usize, i: usize, j: usize) {
+fn eliminate_h(dom_view: &mut [u8], i: usize) {
     let row = i * 9;
     for offset in 0..9 {
         dom_view[row+offset] = 0;
     }
 }
 
-fn eliminate_v(dom_view: &mut [u8], idx: usize, i: usize, j: usize) {
+fn eliminate_v(dom_view: &mut [u8], j: usize) {
     let col = j;
-    for &offset in [0, 9, 18, 27, 36, 45, 54, 63, 72].iter() {
+    for offset in (0..9).map(|x| {x*9}).into_iter() {
         dom_view[col+offset] = 0;
     }
 }
 
-fn eliminate_b(dom_view: &mut [u8], idx: usize, i: usize, j: usize) {
+fn eliminate_b(dom_view: &mut [u8], i: usize, j: usize) {
     let blk = i / 3 * 3 * 9 + j / 3 * 3;
     for &offset in [0, 1, 2, 9, 10, 11, 18, 19, 20].iter() {
         dom_view[blk+offset] = 0;
     }
 }
 
-fn debug_view(dom_view: &[u8]) {
+fn count_possibilities(dom: &[u8], out: &mut u8, i: usize, j: usize) {
+    let mut s = 0u8;
+    let pt = i * 9 + j;
+    for offset in (0..9).map(|x| {x*81}).into_iter() {
+        s += dom[pt+offset];
+    }
+    *out = s;
+}
+
+fn debug_view(v: &[u8]) {
     for i in 0..9 {
-        let r = &dom_view[i*9..(i+1)*9];
+        let r = &v[i*9..(i+1)*9];
         println!("{} {} {} {} {} {} {} {} {}", r[0], r[1], r[2], r[3], r[4], r[5], r[6], r[7], r[8]);
     }
 }
@@ -48,13 +57,24 @@ fn main() {
             if v>0 {
                 let idx = (v - 1) as usize;
                 let view = &mut dom[idx*81..(idx + 1)*81];
-                eliminate_h(view, idx, i, j);
-                eliminate_v(view, idx, i, j);
-                eliminate_b(view, idx, i, j);
+                eliminate_h(view, i);
+                eliminate_v(view, j);
+                eliminate_b(view, i, j);
             }
         }
     }
 
-    debug_view(&dom[(1*81)..(2*81)]);
+    let mut P = vec![0; 81];
+
+    for i in 0..9 {
+        for j in 0..9 {
+            count_possibilities(&dom, &mut P[i*9+j], i, j);
+        }
+    }
+
+    // let k = 1-1;
+    // debug_view(&dom[(k*81)..((k+1)*81)]);
+
+    debug_view(&P);
 
 }
